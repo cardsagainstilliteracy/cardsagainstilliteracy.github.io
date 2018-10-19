73b51f27f988569a2cb5139fd579dcda05d690d0
Kyle Lin <kylejlin.us@gmail.com>
2018-10-18 21:27:42 -0700

    Updates

---
+++ precache-manifest.7eeec2ed9d671c15b67c49762f01fb10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f70f7554e6b9b21e92567c03cbaae0e",
+    "url": "/static/media/settings.5f70f755.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77b5dffe375b8a9fc4c6",
+    "url": "/static/js/main.77b5dffe.chunk.js"
+  },
+  {
+    "revision": "ceb8e4aba671cc8b8d65",
+    "url": "/static/js/1.ceb8e4ab.chunk.js"
+  },
+  {
+    "revision": "77b5dffe375b8a9fc4c6",
+    "url": "/static/css/main.c9d5cf78.chunk.css"
+  },
+  {
+    "revision": "ee63e4e71f1ec0ad8e34aaf179f194b9",
+    "url": "/index.html"
+  }
+];
