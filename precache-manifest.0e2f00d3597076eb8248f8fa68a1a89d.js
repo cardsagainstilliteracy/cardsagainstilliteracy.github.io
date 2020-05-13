9ced2845d63c2e2eceeda689da51f7a51f253d5e
Kyle Lin <kylejlin.us@gmail.com>
2020-05-12 18:25:32 -0700

    Updates

---
+++ precache-manifest.0e2f00d3597076eb8248f8fa68a1a89d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba31a4c43adb816c684eb2ce31caa8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "933fa7c5b1cc2f8f6587",
+    "url": "/static/css/main.7e793187.chunk.css"
+  },
+  {
+    "revision": "256fe2567f44156ab0a9",
+    "url": "/static/js/2.9568cfb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9568cfb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933fa7c5b1cc2f8f6587",
+    "url": "/static/js/main.53252be9.chunk.js"
+  },
+  {
+    "revision": "3d30b7504ab9d7d592a2",
+    "url": "/static/js/runtime-main.563e6cc4.js"
+  },
+  {
+    "revision": "5f70f7554e6b9b21e92567c03cbaae0e",
+    "url": "/static/media/settings.5f70f755.svg"
+  }
+]);
